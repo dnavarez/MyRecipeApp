9d6e4312831780ae4a600e1cc288392d3f677c18
dnavarez <dan_a5trx@hotmail.com>
2023-09-19 04:07:03 +0800

    Add login and register user request for params

---
+++ MyRecipeApp/MyRecipeApp/Models/LoginUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  LoginUserRequest.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/19/23.
+//
+
+import Foundation
+
+struct LoginUserRequest {
+  let email: String
+  let password: String
+}
+
---
+++ MyRecipeApp/MyRecipeApp/Models/RegisterUserRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterUserRequest.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/19/23.
+//
+
+import Foundation
+
+struct RegisterUserRequest {
+  let email: String
+  let password: String
+}

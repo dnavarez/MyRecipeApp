ca9d40fe6619d5b617da2b7062a9df0918cbf613
dnavarez <dan_a5trx@hotmail.com>
2023-09-18 22:02:28 +0800

    Add initial sign in view model

---
+++ MyRecipeApp/MyRecipeApp/Screen/SignInController/SignInViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewModel.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/18/23.
+//
+
+import Foundation
+
+protocol SignInViewModelProtocol {
+  
+}
+
+final class SignInViewModel: SignInViewModelProtocol {
+  init() {
+    
+  }
+}

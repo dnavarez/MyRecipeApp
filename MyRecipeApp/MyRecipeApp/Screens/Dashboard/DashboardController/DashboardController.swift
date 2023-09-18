327f3f1bdf51704036943ae220627f38a6ddf325
dnavarez <dan_a5trx@hotmail.com>
2023-09-19 04:25:25 +0800

    Add initial implementation for dashboard screen

---
+++ MyRecipeApp/MyRecipeApp/Screens/Dashboard/DashboardController/DashboardController.swift
@@ -0,0 +1,28 @@
+//
+//  DashboardController.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/19/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class DashboardController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

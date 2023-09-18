9dfa1deb7bbb2a8b9c1891c8ad2a9735802b1a2a
dnavarez <dan_a5trx@hotmail.com>
2023-09-18 22:25:37 +0800

    Initial add register view model

---
+++ MyRecipeApp/MyRecipeApp/Screen/RegisterController/RegisterViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewModel.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/18/23.
+//
+
+import Foundation
+
+protocol RegisterViewModelProtocol {
+  
+}
+
+final class RegisterViewModel: RegisterViewModelProtocol {
+  init() {
+    
+  }
+}

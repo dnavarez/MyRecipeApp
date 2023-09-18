9719d3ea3a5e752fa5ea925f4371812a661dfcd1
dnavarez <dan_a5trx@hotmail.com>
2023-09-19 04:09:00 +0800

    Refactor app delegate to have shared and app services

---
+++ MyRecipeApp/MyRecipeApp/App/AppDelegate/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/18/23.
+//
+
+import UIKit
+import FirebaseCore
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  static var shared: AppDelegate { UIApplication.shared.delegate as! AppDelegate }
+  
+  var window: UIWindow?
+  var appServices: AppServicesProtocol!
+  
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Do not configure firebase if we are running on unit test
+    if !UIApplication.isRunningTests {
+      FirebaseApp.configure()
+    }
+    
+    let appServices = AppServices()
+    self.appServices = appServices
+    
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    updateRootViewController()
+    self.window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
+

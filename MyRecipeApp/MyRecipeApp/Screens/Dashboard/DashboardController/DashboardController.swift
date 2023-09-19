27f438a41cba1824c7a4a01f8b6f4ca8dac8f72a
dnavarez <dan_a5trx@hotmail.com>
2023-09-19 13:39:10 +0800

    Show sample UI for dashboard screen

--- MyRecipeApp/MyRecipeApp/Screens/Dashboard/DashboardController/DashboardController.swift
+++ MyRecipeApp/MyRecipeApp/Screens/Dashboard/DashboardController/DashboardController.swift
@@ -9,20 +9,78 @@ import UIKit
 import FirebaseAuth
 
 class DashboardController: UIViewController {
+  
+  // MARK: - IBOutlet
+  @IBOutlet weak var tableView: UITableView!
+  
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setups()
+  }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+// MARK: - Setups
+private extension DashboardController {
+  func setups() {
+    setupNavigationBar()
+    setupTableView()
+  }
+  
+  func setupNavigationBar() {
+    navigationItem.title = "My Recipe App"
     
+    let signOutBtn = UIBarButtonItem(
+      image: R.image.iconLogout(),
+      style: .done,
+      target: self,
+      action: #selector(didTapLogout)
+    )
 
-    /*
-    // MARK: - Navigation
+    let addRecipeBtn = UIBarButtonItem(
+      image: R.image.iconPlus(),
+      style: .done,
+      target: self,
+      action: #selector(didTapAddRecipe)
+    )
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    navigationItem.rightBarButtonItems = [signOutBtn, addRecipeBtn]
+  }
+  
+  func setupTableView() {
+    tableView.addPadding()
+    tableView.registerCells(nibs: [R.nib.recipeCell])
+  }
+}
 
+// MARK: - Events
+private extension DashboardController {
+  @objc
+  func didTapLogout() {
+    try? Auth.auth().signOut()
+  }
+  
+  @objc
+  func didTapAddRecipe() {
+    try? Auth.auth().signOut()
+  }
+}
+
+// MARK: - Delegates
+extension DashboardController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(
+      withIdentifier: R.nib.recipeCell.name
+    ) as? RecipeCell
+    else { return UITableViewCell() }
+    
+    return cell
+  }
 }

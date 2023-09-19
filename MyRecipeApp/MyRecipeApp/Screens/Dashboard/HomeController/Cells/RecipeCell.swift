2b1ff98ef1cd18fee5d80ecaae23826651cff4de
dnavarez <dan_a5trx@hotmail.com>
2023-09-19 17:48:16 +0800

    Add iboutlets for recipe cell

--- MyRecipeApp/MyRecipeApp/Screens/Dashboard/HomeController/Cells/RecipeCell.swift
+++ MyRecipeApp/MyRecipeApp/Screens/Dashboard/HomeController/Cells/RecipeCell.swift
@@ -8,16 +8,22 @@
 import UIKit
 
 class RecipeCell: UITableViewCell {
+  
+  // MARK: - IBOutlet
+  @IBOutlet weak var recipeImageView: UIImageView!
+  @IBOutlet weak var recipeNameLabel: UILabel!
+  @IBOutlet weak var ownerLabel: UILabel!
+  
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+}
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
+// MARK: - Setups
+private extension RecipeCell {
+  func setups() {}
 }

a14cf153ddffecf6be261931fd9cf12a1f0eade5
dnavarez <dan_a5trx@hotmail.com>
2023-09-19 23:48:52 +0800

    Add recipe instruction cell

---
+++ MyRecipeApp/MyRecipeApp/Screens/Dashboard/AddRecipeController/Cells/RecipeInstructionCell/RecipeInstructionCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeInstructionCell.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/19/23.
+//
+
+import UIKit
+
+class RecipeInstructionCell: UITableViewCell {
+  @IBOutlet weak var instructionLabel: UILabel!
+}

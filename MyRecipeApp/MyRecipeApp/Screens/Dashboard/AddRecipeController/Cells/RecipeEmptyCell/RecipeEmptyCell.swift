ce5eff8cb9dae321d545edd9cd9457e5afc4b740
dnavarez <dan_a5trx@hotmail.com>
2023-09-19 23:49:17 +0800

    Add recipe empty cell

---
+++ MyRecipeApp/MyRecipeApp/Screens/Dashboard/AddRecipeController/Cells/RecipeEmptyCell/RecipeEmptyCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeEmptyCell.swift
+//  MyRecipeApp
+//
+//  Created by Dan Navarez on 9/19/23.
+//
+
+import UIKit
+
+class RecipeEmptyCell: UITableViewCell {
+  @IBOutlet weak var messageLabel: UILabel!
+}
